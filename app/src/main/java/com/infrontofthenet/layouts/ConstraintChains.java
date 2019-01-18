ac80a550fdaa4b93c680a31ff7f809a618b80382
ifotn <rich@infrontofthenet.com>
2019-01-18 08:41:37 -0500

    created empty constraint chains layout

---
+++ app/src/main/java/com/infrontofthenet/layouts/ConstraintChains.java
@@ -0,0 +1,13 @@
+package com.infrontofthenet.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConstraintChains extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint_chains);
+    }
+}
